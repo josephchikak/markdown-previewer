d7f18e9152c9947d52ce09e15c19fadae5db7e08
raytheboffin <raytheboffin@gmail.com>
2022-09-27 21:49:42 +0300

    another one

---
+++ build/precache-manifest.4e990ae8699452232e3369ca85846221.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc21656779c195e17449",
+    "url": "/josephchikak/markdown-previewer/static/css/main.2610cf25.chunk.css"
+  },
+  {
+    "revision": "cc21656779c195e17449",
+    "url": "/josephchikak/markdown-previewer/static/js/main.cc216567.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/josephchikak/markdown-previewer/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "004b052b1c1553b3964b",
+    "url": "/josephchikak/markdown-previewer/static/css/2.c1cfa8ec.chunk.css"
+  },
+  {
+    "revision": "004b052b1c1553b3964b",
+    "url": "/josephchikak/markdown-previewer/static/js/2.004b052b.chunk.js"
+  },
+  {
+    "revision": "a9ef0dec737d2aca4e35",
+    "url": "/josephchikak/markdown-previewer/static/js/runtime~main.a9ef0dec.js"
+  },
+  {
+    "revision": "e116a0e12e9d877df7aa5b97cd22b336",
+    "url": "/josephchikak/markdown-previewer/index.html"
+  }
+];
